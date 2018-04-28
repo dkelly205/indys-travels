6e7786874fb8dd3764f510f8f330bef374604fb8
John McCollum <johnvmccollum@gmail.com>
2018-04-28 20:30:06 +0100

    Actual locations

--- src/config/Locations.js
+++ src/config/Locations.js
@@ -1,8 +1,13 @@
 export const Raiders = [
-	{lat: 0, lng: 0, text: "Point 1", zoom: 4},
-	{lat: 1, lng: 1, text: "Point 2", zoom: 11},
-	{lat: 2, lng: 2, text: "Point 3", zoom: 8},
-	{lat: 3, lng: 3, text: "Point 4", zoom: 3},
-	{lat: 4, lng: 4, text: "Point 5", zoom: 5},
-	{lat: 55, lng: -4, text: "Point 6", zoom: 1}
+	{lat: 22.0964, lng: -159.5261, text: "Kauai, Hawaii", zoom: 10},
+	{lat: 22.1453, lng: -159.3156, text: "Anahola, Hawaii", zoom: 10},
+	{lat: 21.9507, lng: -159.4240, text: "Kipu Ranch", zoom: 10},
+	{lat: 37.9735, lng: -122.5311, text: "San Rafael, California", zoom: 10},
+	{lat: 37.7792597, lng: -122.4214533, text: "San Francisco City Hall", zoom: 16},
+	{lat: 37.9816207, lng: -121.3166308, text: "University of the Pacific, Stockton, California", zoom: 16},
+	{lat: 46.1603, lng: -1.1511, text: "La Rochelle, France", zoom: 6},
+	{lat: 51.6480, lng: -0.4736, text: "Royal Masonic School for Girls", zoom: 16},
+	{lat: 51.6581, lng: -0.269, text: "Elstree Studios", zoom: 15},
+	{lat: 34.0335143, lng: 9.6476724, text: "Sidi Bouhlel", zoom: 10},
+	{lat: 35.6759, lng: 10.0919, text: "Kairouan", zoom: 10},
 ];
